e8364bf8d3dfc2a996050ff2cb545a94c10fbd40
Joshi <joshua@lipps.de>
2025-04-07 16:29:51 +0200

    store previous rounds of game

--- frontend/ts/game.ts
+++ frontend/ts/game.ts
@@ -3,21 +3,77 @@ function createArray<T>(size: number, value: T): T[] {
 }
 
 class Game {
+  private previous_rounds: Round[] = [];
+  private current_round: Round;
+  private points: number[];
   private player_amount: number;
-  private points: number[]
-  private picked_coins: number[]
-  private tricks_won: number[]
-  private current_round: number = 1;
   private starting_player: number;
 
   constructor(player_amount: number) {
     if(player_amount < MIN_PLAYERS)
       errorNotEnoughPlayers(player_amount);
     this.player_amount = player_amount;
+    this.starting_player = random(0, player_amount);
+    this.current_round = new Round(player_amount, 1);
+    this.points = createArray<number>(player_amount, 0);
+  }
+
+  public pickedCoin(player: number): boolean {
+    return this.current_round.pickedCoin(player);
+  }
+  public unpickedCoin(player: number): boolean {
+    return this.current_round.unpickedCoin(player);
+  }
+  public trickWon(player: number): boolean {
+    return this.current_round.trickWon(player);
+  }
+  public trickLost(player: number): boolean {
+    return this.current_round.trickLost(player);
+  }
+
+  public nextRound(): number {
+    let pointsChange = this.current_round.complete();
+    this.points = this.points.map((points, index) => points + pointsChange[index]);
+
+    this.starting_player = (this.starting_player + 1) % this.player_amount;
+    this.previous_rounds.push(this.current_round);
+    this.current_round = this.current_round.nextRound();
+    return this.current_round.getCurrentRound();
+  }
+
+  public getPoints(): number[] {
+    return this.points;
+  }
+  public getTotalTricksWon(): number {
+    return this.current_round.getTotalTricksWon();
+  }
+  public getPreviousRounds(): Round[] {
+    return this.previous_rounds;
+  }
+  public getCurrentRound(): number {
+    return this.current_round.getCurrentRound();;
+  }
+  public getStartingPlayer(): number {
+    return this.starting_player;
+  }
+  public getPlayerAmount(): number {
+    return this.player_amount;
+  }
+}
+
+class Round {
+  private player_amount: number;
+  private points: number[]
+  private picked_coins: number[]
+  private tricks_won: number[]
+  private current_round: number = 1;
+
+  public constructor(player_amount: number, current_round: number) {
+    this.player_amount = player_amount;
+    this.current_round = current_round;
     this.points = createArray<number>(player_amount, 0);
     this.picked_coins = createArray<number>(player_amount, 0);
     this.tricks_won = createArray<number>(player_amount, 0);
-    this.starting_player = random(0, player_amount);
   }
 
   public pickedCoin(player: number): boolean {
@@ -51,44 +107,35 @@ class Game {
     this.tricks_won[player]--;
     return true;
   }
+
+  private calculatePointsForPlayer(player: number) {
+    let picked_coins = this.picked_coins[player];
+    let tricks_won = this.tricks_won[player];
+    if(tricks_won == picked_coins)
+      return tricks_won + 2;
+    else
+      return Math.abs(this.tricks_won[player] - picked_coins);
+  }
+  public complete(): number[] {
+    for(let i = 0; i < this.player_amount; i++) {
+      this.points[i] = this.calculatePointsForPlayer(i);
+    }
+    return this.points;
+  }
+
   public getTricksWon(player: number): number  {
     return this.tricks_won[player];
   }
   public getTotalTricksWon(): number {
     return this.tricks_won.reduce((total, cur) => total + cur);
   }
-
-  public nextRound(): number {
-    for(let i = 0; i < this.player_amount; i++) {
-      this.calculatePointsForPlayer(i);
-      this.tricks_won[i] = 0;
-      this.picked_coins[i] = 0;
-    }
-    this.starting_player = (this.starting_player + 1) % this.player_amount;
-    return ++this.current_round;
-  }
-
-  private calculatePointsForPlayer(player: number) {
-    let picked_coins = this.picked_coins[player];
-    let points_delta = this.tricks_won[player];
-    if(this.tricks_won[player] == picked_coins)
-      points_delta += 2;
-    else
-      points_delta -= Math.abs(this.tricks_won[player] - picked_coins);
-
-    this.points[player] += points_delta;
-  }
-
-  public getCurrentRound(): number {
-    return this.current_round;
-  }
-  public getStartingPlayer(): number {
-    return this.starting_player;
-  }
   public getPoints(): number[] {
     return this.points;
   }
-  public getPlayerAmount(): number {
-    return this.player_amount;
+  public nextRound(): Round {
+    return new Round(this.player_amount, this.current_round + 1);
+  }
+  public getCurrentRound(): number {
+    return this.current_round;
   }
 }
